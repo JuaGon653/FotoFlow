912cdbc3ee690cd5f6b67799cda8f17afc0fe84a
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2023-02-09 14:09:08 -0600

    created post model

---
+++ server/models/Post.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    caption: {
+        type: String
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
