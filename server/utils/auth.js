bb803c67375541f52b63503cc0a39b77e85fb8f1
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2023-02-09 14:40:54 -0600

    implemented a verifyToken method

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,5 +6,23 @@ module.exports = {
     signToken: function({ email, username, _id }) {
         const payload = { email, username, _id };
         return jwt.sign({ data: payload }, secret, { expiresIn: '1d' })
+    },
+    verifyToken: function({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        if(req.header.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token) {
+            return res.status(401).send({ message: 'Unauthorized' });
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: '1d' });
+            req.user = data;
+        } catch(error) {
+            return res.status(401).send({ message: 'Unauthorized' });
+        }
     }
 }
