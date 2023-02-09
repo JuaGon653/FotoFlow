75da837b9ae343377d1478ae448ec51b44862474
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2023-02-09 16:15:49 -0600

    created basic queries

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,57 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        fullName: String!
+        profileImage: String
+        followers: [User]
+        following: [User]
+        posts: [Post]
+        createdAt: String
+    }
+
+    type Post {
+        _id: ID!
+        caption: String!
+        photo: String!
+        likes: [User]
+        comments: [Comment]
+        user: User
+        createdAt: String
+    }
+
+    type Comment {
+        _id: ID!
+        text: String!
+        user: User
+        post: Post
+        createdAt: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        find_email(email: String!): User
+        find_username(username: String!): User
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): User
+
+        createPost(user: ID!, caption: String!, photo: String!): Post
+
+        createComment(user: ID!, post: ID!, text: String!): Comment
+
+        addUser(username: String!, email: String!, password: String!): User
+    }
+`;
+
+module.exports = typeDefs;
