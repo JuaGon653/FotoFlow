257efb65f4c27b51c337fe9f60b812513863d8e9
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2023-02-09 14:04:06 -0600

    created user model

---
+++ server/models/User.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  profileImage: {
+    type: String,
+  },
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+})
+
+UserSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+}
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
