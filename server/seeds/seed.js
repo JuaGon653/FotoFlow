f948cca357fbd0ae8bdd5d1054d9b39e773580ee
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2023-02-13 15:15:26 -0600

    seeds hardcoded post data

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,6 +2,7 @@ const db = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
+const postData = require('./postData.json');
 
 db.once('open', async () => {
     await User.deleteMany({});
@@ -9,6 +10,16 @@ db.once('open', async () => {
     await Comment.deleteMany({});
 
     const userbase = await User.create(userData);
+    const postbase = await Post.create(postData);
+
+    for (newPost of postbase) {
+        const sequenceUser = userbase[postbase.indexOf(newPost)];
+        newPost.user = sequenceUser._id;
+        await newPost.save();
+
+        sequenceUser.posts = [newPost._id];
+        await sequenceUser.save();
+    }
 
     console.log('Userbase seeded!');
     process.exit(0);
