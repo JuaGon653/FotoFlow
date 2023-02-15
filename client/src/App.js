507946451f0799c737a17fe4e13b67cc44cb67c2
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2023-02-15 14:47:49 -0600

    app uses apollo client and react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,60 @@
 import React, { useState } from 'react';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import Nav from './components/Nav';
 import Home from './pages/Home';
 
-export default function App() {
-  const [currentPage, setCurrentPage] = useState('Home');
+import UserProvider from './utils/UserContext';
 
-  const renderPage = function() {
-    switch (currentPage) {
-      case 'Home':
-        return <Home />
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
     }
   };
+});
 
-  const handlePageChange = function(page) {
-    setCurrentPage(page);
-  };
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+export default function App() {
+  // const [currentPage, setCurrentPage] = useState('Home');
+
+  // const renderPage = function() {
+  //   switch (currentPage) {
+  //     case 'Home':
+  //       return <Home />
+  //   }
+  // };
+
+  // const handlePageChange = function(page) {
+  //   setCurrentPage(page);
+  // };
 
   return (
-    <div>
-      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
-      {renderPage()}
-    </div>
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <Router>
+          <div>
+            <Nav />
+            <Routes>
+              <Route path='/' element={<Home />} />
+            </Routes>
+          </div>
+        </Router>
+      </UserProvider>
+    </ApolloProvider>
   );
 };
