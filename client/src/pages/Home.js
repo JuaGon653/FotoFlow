b1f8381e59aa2eb54aee6ff8e92f63052d4e9d59
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2023-02-15 14:48:32 -0600

    queries the database for the post data

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { FIND_POSTS } from '../utils/queries';
+
+import PostList from '../components/PostList';
 
 export default function Home() {
+    const { loading, data: allPosts } = useQuery(FIND_POSTS);
+    const posts = allPosts?.posts || [];
+    console.log(posts);
+
     return (
-        <h1>Hi</h1>
+        <div className="grid-x">
+            <div className="cell small-4"></div>
+            <div className="cell small-4 text-center">
+                {loading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <PostList posts={posts} />
+                )}
+            </div>
+            <div className="cell small-4"></div>
+        </div>
     );
 };
